21a08bcd188155f6e68b0dcc2c3869005a0caff7
Manuel Gutekunst <gutekunst@medialesson.de>
2013-12-28 11:29:31 +0100

    Context Action to switch true and false and vice versa

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,25 @@
+﻿using System.Reflection;
+using JetBrains.ActionManagement;
+using JetBrains.Application.PluginSupport;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ResharperPlugins")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Acme Corp.")]
+[assembly: AssemblyProduct("ResharperPlugins")]
+[assembly: AssemblyCopyright("Copyright © Acme Corp., 2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
+[assembly: ActionsXml("ResharperPlugins.Actions.xml")]
+
+// The following information is displayed by ReSharper in the Plugins dialog
+[assembly: PluginTitle("ResharperPlugins")]
+[assembly: PluginDescription("")]
+[assembly: PluginVendor("Acme Corp.")]

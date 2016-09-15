39324f9eaf9b176edc8498be4656a669fe904cc1
Manuel Gutekunst <Manuel.Gutekunst@generic.de>
2016-09-15 15:51:56 +0200

    added Availability Test for TrueFalseSwitcher

---
+++ LittleHelpers.RS9.Tests/LittleHelpersTestAssembly.cs
@@ -0,0 +1,13 @@
+﻿using JetBrains.TestFramework;
+using Littlehelpers.RS9.Tests;
+using NUnit.Framework;
+
+#pragma warning disable 618
+[assembly: TestDataPathBase(@".\data")]
+#pragma warning restore 618
+// ReSharper disable once CheckNamespace
+
+[SetUpFixture]
+class LittleHelpersTestAssembly : ExtensionTestEnvironmentAssembly<LittleHelpersTestEnvironmentZone>
+{
+}
---
+++ LittleHelpers.RS9.Tests/data/Intentions/ContextActions/TrueFalseSwitcherTests/Availability.cs
@@ -0,0 +1,15 @@
+﻿namespace Littlehelpers.RS9.Tests.data.Intentions.ContextActions.TrueFalseSwitcherTests
+{
+    public class Availability
+    {
+        public void TestMethodTrue()
+        {
+            var b = tr{on}ue;
+        }
+
+        public void TestMe{off}thodFalse()
+        {
+            var b = fa{on}lse;
+        }
+    }
+}
